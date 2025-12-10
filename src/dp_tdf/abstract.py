a8ed2aa97aa3a4ed8b0998edd1b6e337fae69fdb
Jakub Sikora <jakubsikora515@gmail.com>
2025-12-10 20:08:43 +0100

    Change STFT to return real-valued output
    Fixing patch so the DTTNet model works with Torch version 2.9.0

--- src/dp_tdf/abstract.py
+++ src/dp_tdf/abstract.py
@@ -130,7 +130,7 @@ class AbstractModel(LightningModule):
         '''
         dim_b = x.shape[0]
         x = x.reshape([dim_b * self.audio_ch, -1]) # (batch*c, 261120)
-        x = torch.stft(x, n_fft=self.n_fft, hop_length=self.hop_length, window=self.window, center=True, return_complex=True) # (batch*c, 3073, 256, 2)
+        x = torch.stft(x, n_fft=self.n_fft, hop_length=self.hop_length, window=self.window, center=True, return_complex=False) # (batch*c, 3073, 256, 2)
         x = x.permute([0, 3, 1, 2]) # (batch*c, 2, 3073, 256)
         x = x.reshape([dim_b, self.audio_ch, 2, self.n_bins, -1]).reshape([dim_b, self.audio_ch * 2, self.n_bins, -1]) # (batch, c*2, 3073, 256)
         return x[:, :, :self.dim_f] # (batch, c*2, 2048, 256)
